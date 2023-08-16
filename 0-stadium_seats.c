8961ceaafe7a905b4ddbc4f525b30209005c19bf
brk-a <fnjakai@outlook.com>
2023-08-16 04:47:36 +0300

    stadium_seats: acts atypically

---
+++ 0-stadium_seats.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, int *argv){
+    if(argc!=2){
+        printf("Usage: ./stadium_seats row\n");
+        return 0;
+    }
+    int i = argv[1];
+    stadium_seats(i);
+
+    return 0;
+}
+
+int stadium_seats(row)
+int row;
+{
+    for(int i=row; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
