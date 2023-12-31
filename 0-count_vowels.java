56559c6171d04f99adcf20ece6ede7e24d1fcd96
brk-a <fnjakai@outlook.com>
2023-12-31 04:22:38 +0300

    ..

--- 0-count_vowels.java
+++ 0-count_vowels.java
@@ -1,5 +1,7 @@
 public class VowelCounter {
     public static String vowelCounter(){}
     
-    public static void main(String args[]){}
+    public static void main(String args[]){
+        
+    }
 }
