06e60033b53e89248db070cc5982b02cf9c8195b
brk-a <fnjakai@outlook.com>
2024-01-01 03:11:44 +0300

    count_vowels

--- 0-count_vowels.java
+++ 0-count_vowels.java
@@ -1,7 +1,32 @@
 public class VowelCounter {
-    public static String vowelCounter(){}
+    public static int vowelCounter(char[] letters){
+        int count = 0;
+
+        for(char c : letters) {
+            switch (c) {
+                case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        count++;
+            break;
+            default:
+            }
+        }
+
+        return count;
+    }
     
     public static void main(String args[]){
-        
+        System.out.println("enter text...");
+        Scanner reader = new Scanner(System.in)
+
+        String input = reader.nextLine();
+        char[] letters = input.toCharArray();
+
+        int vowelCount = vowelCounter(letters);
+
+        System.out.println("Number of vowels in string [%s] is %d", input, vowelCount);
     }
 }
