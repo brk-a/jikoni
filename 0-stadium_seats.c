ebb7a02d66d3af7526159b1e4a8f690358aa6b3f
brk-a <fnjakai@outlook.com>
2024-05-21 08:55:18 +0300

    stadium_seats: finally done

--- 0-stadium_seats.c
+++ 0-stadium_seats.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
-
-int main(int argc, int *argv){
-    if(argc!=2){
-        printf("Usage: ./stadium_seats n_row\n");
-        return 0;
-    }
-    int i = argv[1];
-    stadium_seats(i);
-
-    return 0;
-}
+#include <stdlib.h>
 
 int stadium_seats(row)
 int row;
 {
-    for(int i=row; i>=1; i--){
-        for(int j=1; j<=i; j++){
+    for(int i=row; i>0; i--){
+        for(int j=0; j<i; j++){
             putchar('*');
         }
         putchar('\n');
     }
     return 0;
 }
+
+int main(int argc, char *argv[]){
+    if(argc!=2){
+        printf("Usage: ./stadium_seats n_row\n");
+        return 0;
+    }
+    int i =  atoi(argv[1]);
+    stadium_seats(i);
+    
+    return 0;
+}
+
 
 /**
 stadium_seats(5)
