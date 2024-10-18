968dfe3eb3eb7d91bb854bbada2dca0470d53bb6
brk-a <fnjakai@outlook.com>
2024-10-18 09:02:03 +0300

    tail_recursion

--- 1-tail_recursion.go
+++ 1-tail_recursion.go
@@ -9,15 +9,15 @@ import (
 /**
 *Fibonacci using tail recursion instead of trivial recursion
 * fib: fn to find fib of n
-*@n: positive float
-*return: float: fib of n
+*@n: positive int
+*return: int: fib of n
 *goNuts: helper fuction; eliminates the need to have trivial recursion
-*@n: positive float
+*@n: positive int
 *@a: current fib number
 *@b: next fib number
 */
 
-func goNuts(n float64, a float64, b float64) float64 {
+func goNuts(n int64, a int64, b int64) int64 {
 	if(n == 0){
 		return a;
 	} else if(n==1) {
@@ -27,7 +27,7 @@ func goNuts(n float64, a float64, b float64) float64 {
 	}
 }
 
-func fib(n float64) float64 {
+func fib(n int64) int64 {
 	return goNuts(n, 0, 1);
 }
 
@@ -37,7 +37,7 @@ func main() {
 		return
 	}
 
-	n, err := strconv.ParseFloat(os.Args[1], 64);
+	n, err := strconv.ParseInt(os.Args[1], 0, 64);
 	if err!=nil || n < 0 {
         fmt.Printf("Error: %s is not a positive integer\n", os.Args[1]);
         return
